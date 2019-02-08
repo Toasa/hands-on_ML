d38e59f2b547e0f906d4072fe4574937f70c0b79
toasa <atsushi.tohyama.160.333@gmail.com>
2019-02-08 15:48:32 +0900

    I read ch03 now

--- ch01/ex1_1/main.py
+++ ch01/ex1_1/main.py
@@ -5,3 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sklearn.linear_model
+
+oecd = pd.read_csv("oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t')
+
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+X = np.c_[country_stats["GDP per capita"]]
+Y = np.c_[country_stats["life satisfaction"]]
